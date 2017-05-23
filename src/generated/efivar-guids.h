3daa20131336bc8af7cbf784a7d302f172da1127
sgnanase <sundar.gnanasekaran@intel.com>
2017-05-23 22:42:57 +0530

    [Android-IA] Port missing generated header/bin files
    Remove unwanted .gitignore from src/ folder to keep track
    of generated files.
    
    Jira : None
    Test : Code compilation is successful

---
+++ src/generated/efivar-guids.h
@@ -0,0 +1,38 @@
+#ifndef EFIVAR_GUIDS_H
+#define EFIVAR_GUIDS_H 1
+
+extern const efi_guid_t efi_guid_empty;
+extern const efi_guid_t efi_guid_zero;
+extern const efi_guid_t efi_guid_sha512;
+extern const efi_guid_t efi_guid_redhat;
+extern const efi_guid_t efi_guid_sha224;
+extern const efi_guid_t efi_guid_lenovo_boot_menu;
+extern const efi_guid_t efi_guid_x509_sha256;
+extern const efi_guid_t efi_guid_rsa2048;
+extern const efi_guid_t efi_guid_lenovo;
+extern const efi_guid_t efi_guid_lenovo_diag;
+extern const efi_guid_t efi_guid_x509_sha512;
+extern const efi_guid_t efi_guid_pkcs7_cert;
+extern const efi_guid_t efi_guid_shim;
+extern const efi_guid_t efi_guid_lenovo_rescue;
+extern const efi_guid_t efi_guid_rsa2048_sha1;
+extern const efi_guid_t efi_guid_x509_sha384;
+extern const efi_guid_t efi_guid_lenovo_setup;
+extern const efi_guid_t efi_guid_microsoft;
+extern const efi_guid_t efi_guid_lenovo_2;
+extern const efi_guid_t efi_guid_sha1;
+extern const efi_guid_t efi_guid_lenovo_me_config;
+extern const efi_guid_t efi_guid_global;
+extern const efi_guid_t efi_guid_x509_cert;
+extern const efi_guid_t efi_guid_rsa2048_sha256_cert;
+extern const efi_guid_t efi_guid_lenovo_diag_splash;
+extern const efi_guid_t efi_guid_redhat_2;
+extern const efi_guid_t efi_guid_lenovo_msg;
+extern const efi_guid_t efi_guid_sha256;
+extern const efi_guid_t efi_guid_shell;
+extern const efi_guid_t efi_guid_security;
+extern const efi_guid_t efi_guid_rsa2048_sha256;
+extern const efi_guid_t efi_guid_sha384;
+extern const efi_guid_t efi_guid_lenovo_startup_interrupt;
+
+#endif /* EFIVAR_GUIDS_H */
